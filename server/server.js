8835cf379675b359b3dd2c183b7070f7f12b24a6
Teodor Ande Elstad <teodor@andeelstad.com>
2017-09-11 22:32:30 +0200

    Will express allow credentials?

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ var config = require("cheslie-config"),
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
